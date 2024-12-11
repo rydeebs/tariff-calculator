9bb17c0af11de5f1e84f3d2751398ee7acc66f59
rydeebs <rnd5061@gmail.com>
2024-12-10 22:27:01 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
+
